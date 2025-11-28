2e90f197630c70582031bf37909fe4ec80a8be5b
Unni T A <unnita1235@gmail.com>
2025-11-28 21:04:52 +0530

    Update properties.js

--- backend/routes/properties.js
+++ backend/routes/properties.js
@@ -10,10 +10,13 @@ const { authMiddleware, roleMiddleware } = require('../middleware/auth');
 
 const router = express.Router();
 
+// Owner only routes
 router.post('/', authMiddleware, roleMiddleware(['Owner']), createProperty);
-router.get('/', getProperties);
-router.get('/:id', getPropertyById);
 router.put('/:id', authMiddleware, roleMiddleware(['Owner']), updateProperty);
 router.delete('/:id', authMiddleware, roleMiddleware(['Owner']), deleteProperty);
+
+// Public routes
+router.get('/', getProperties);
+router.get('/:id', getPropertyById);
 
 module.exports = router;
