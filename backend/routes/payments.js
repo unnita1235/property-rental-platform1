5bb2ce384848789336cbb4d621f308ccdddacce6
Unni T A <unnita1235@gmail.com>
2025-11-28 21:05:28 +0530

    Update payments.js

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -4,6 +4,7 @@ const { authMiddleware, roleMiddleware } = require('../middleware/auth');
 
 const router = express.Router();
 
+// Customer only routes
 router.post('/', authMiddleware, roleMiddleware(['Customer']), recordPayment);
 router.get('/', authMiddleware, roleMiddleware(['Customer']), getPayments);
 
