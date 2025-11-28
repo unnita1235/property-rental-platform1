e44123b15414683eb08b016d24aad2b1e5546091
Unni T A <unnita1235@gmail.com>
2025-11-28 21:03:30 +0530

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,20 +3,17 @@ const Property = require('./Property');
 const Booking = require('./Booking');
 const Payment = require('./Payment');
 
-// User - Property association
+// Define associations
 User.hasMany(Property, { foreignKey: 'ownerId', as: 'properties' });
 Property.belongsTo(User, { foreignKey: 'ownerId', as: 'owner' });
 
-// User - Booking association
 User.hasMany(Booking, { foreignKey: 'customerId', as: 'bookings' });
 Booking.belongsTo(User, { foreignKey: 'customerId', as: 'customer' });
 
-// Property - Booking association
 Property.hasMany(Booking, { foreignKey: 'propertyId', as: 'bookings' });
 Booking.belongsTo(Property, { foreignKey: 'propertyId', as: 'property' });
 
-// Booking - Payment association
 Booking.hasOne(Payment, { foreignKey: 'bookingId', as: 'payment' });
 Payment.belongsTo(Booking, { foreignKey: 'bookingId', as: 'booking' });
 
-module.exports = { User, Property, Booking, Payment };
+module.exports = { User, Property, Booking, Payment, sequelize: require('../config/database') };
